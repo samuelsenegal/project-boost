b1d135d5e096a1fd1b064f2b26a5a162a014cf6f
Wassamp <55412772+samuelsenegal@users.noreply.github.com>
2023-03-06 14:05:49 -0600

    Desktop Changes
    Completed course

--- Project Boost/Assets/Scripts/Oscillator.cs
+++ Project Boost/Assets/Scripts/Oscillator.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class Oscillator : MonoBehaviour
 {
     Vector3 _startPosition;
-    [SerializeField] Vector3 _movementVector;
+    [SerializeField] Vector3 _movementVector; // Controls the endpoint in 3D Space
     [SerializeField] [Range(0,1)] float _movementFactor;
-    [SerializeField] float _period = 2f;
+    [SerializeField] float _period = 4f; // Controls speed of movement
 
     // Start is called before the first frame update
     void Start()
@@ -18,8 +18,13 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float _cycles = Time.time / _period;
-        const float TAU = Mathf.PI * 2;
+        if (_period  <= Mathf.Epsilon) { return; }
+        float _cycles = Time.time / _period; // Continually grows over time
+
+        const float TAU = Mathf.PI * 2; // Constant value: 6.281 denotes 2 radians (full domain of Sin)
+        float _rawSinWave = Mathf.Sin(TAU * _cycles); // Going from [-1,1] (full range of Sin)
+
+        _movementFactor = (_rawSinWave + 1f) / 2f; // Converts value to [0,1]
 
         Vector3 _offset = _movementVector * _movementFactor;
         transform.position = _startPosition + _offset;
