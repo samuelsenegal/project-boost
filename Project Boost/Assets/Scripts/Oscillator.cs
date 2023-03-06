465782c75aa2edb93defeda0f2025e117047ac73
samuelsenegal <samuelsenegal23@gmail.com>
2023-03-06 11:45:57 -0600

    Laptop Changes
    Added Sin wave functionality

--- Project Boost/Assets/Scripts/Oscillator.cs
+++ Project Boost/Assets/Scripts/Oscillator.cs
@@ -7,6 +7,7 @@ public class Oscillator : MonoBehaviour
     Vector3 _startPosition;
     [SerializeField] Vector3 _movementVector;
     [SerializeField] [Range(0,1)] float _movementFactor;
+    [SerializeField] float _period = 2f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +18,9 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float _cycles = Time.time / _period;
+        const float TAU = Mathf.PI * 2;
+
         Vector3 _offset = _movementVector * _movementFactor;
         transform.position = _startPosition + _offset;
     }
